5e0476eb357d57f027569e1b0afa0e05580d989b
koike <koike@NA21032.nature.local>
2021-08-06 12:09:28 +0900

    従業員newアクションの作成

--- src/main/java/constants/ForwardConst.java
+++ src/main/java/constants/ForwardConst.java
@@ -23,6 +23,7 @@ public enum ForwardConst {
 	CMD_LOGIN("login"),
 	CMD_LOGOUT("logout"),
 	CMD_NEW("entryNew"),
+	CMD_CREATE("create"),
 	CMD_EDIT("edit"),
 	CMD_UPDATE("update"),
 	CMD__DESTROY("destroy"),
